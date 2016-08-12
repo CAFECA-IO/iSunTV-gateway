fd32231d2f6352865ecff55853f3c95cac3b654f
Luphia Chang <luphia@outlook.com>
2016-08-12 16:03:03 +0800

    Edit: Add payment plan data processing

--- bots/Payment.js
+++ bots/Payment.js
@@ -4,6 +4,33 @@ const braintree = require("braintree");
 
 var logger;
 
+// PaymentPlan.type: 1 = 單租, 2 = 套餐, 3 = subscribe
+var formatPaymentPlan = function (data) {
+	data.type = parseInt(data.type);
+	data.fee = data.fee || {};
+	if(!(data.type > 0 && data.type <= 3)) { data.type = 1; }
+	if(!(data.fee.price > 0)) { data.fee.price = 0; }
+	if(!(data.fee.currency)) { data.fee.currency = 'USD'; }
+	if(!Array.isArray(data.programs)) { data.programs = []; }
+	var PaymentPlan = {
+		type: data.type,
+		title: data.title || 'PlanX',
+		fee: data.fee,
+		programs: data.programs
+	};
+	return PaymentPlan;
+};
+var descPaymentPlan = function (data, detail) {
+	var PaymentPlan = {
+		ppid: data._id,
+		type: data.type,
+		title: data.title,
+		fee: data.fee
+	};
+	if(!!detail) { PaymentPlan.programs = data.programs; }
+	return PaymentPlan;
+};
+
 var Bot = function (config) {
 	if (!config) config = {};
 	this.init(config);
@@ -24,6 +51,8 @@ Bot.prototype.init = function (config) {
 Bot.prototype.start = function () {
 
 };
+
+Bot.prototype.createBasicPaymentPlan
 
 Bot.prototype.generateClientToken = function (options, cb) {
 	var token = this.gateway.clientToken.generate({}, function (e, d) {
