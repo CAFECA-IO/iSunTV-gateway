17711a9c19ed1a513a5f3f8a840877decefd3613
Luphia Chang <luphia@outlook.com>
2016-08-18 19:52:55 +0800

    Enhance: Add fill payment information mechanism

--- bots/Payment.js
+++ bots/Payment.js
@@ -12,7 +12,7 @@ var formatPaymentPlan = function (data) {
 	if (Array.isArray(data)) { return data.map(formatPaymentPlan); }
 	data.type = parseInt(data.type);
 	data.fee = data.fee || {};
-	if(!(data.type > 0 && data.type <= 3)) { data.type = 1; }
+	if(!(data.type > 0 && data.type <= 6)) { data.type = 1; }
 	if(!(data.fee.price > 0)) { data.fee.price = 0; }
 	if(!(data.fee.currency)) { data.fee.currency = 'USD'; }
 	if(!Array.isArray(data.programs)) { data.programs = []; }
@@ -41,7 +41,13 @@ var descPaymentPlan = function (data, detail) {
 		title: data.title,
 		fee: data.fee
 	};
-	if(!!detail) { PaymentPlan.programs = data.programs; }
+	if(!!detail) { 
+		PaymentPlan.programs = data.programs;
+		PaymentPlan.ticket = data.ticket;
+		PaymentPlan.visible = data.visible;
+		PaymentPlan.enable = data.enable;
+	}
+	delete PaymentPlan._id
 	return PaymentPlan;
 };
 
@@ -64,6 +70,27 @@ var descOrder = function (data) {
 	data.oid = data._id;
 	delete data._id;
 	return data;
+};
+
+var formatTicket = function (data) {
+	if(Array.isArray(data)) { return data.map(formatTicket); }
+	var ticket = {
+		type: data.type,
+		oid: data.oid,
+		uid: data.uid,
+		programs: data.programs,
+		enable: false,
+		expire: data.expire,
+		duration: data.duration,
+		ctime: data.ctime,
+		mtime: data.mtime,
+		atime: data.atime
+	}
+	return ticket;
+};
+
+var isPlayable = function (rule, pid) {
+	return new RegExp(rule).test(pid);
 };
 
 var Bot = function (config) {
@@ -135,7 +162,7 @@ Bot.prototype.initialPaymentPlan = function (options, cb) {
 				enable: true,
 				visible: true,
 				ticket: {
-					expire: 86400 * 1000 * 7,
+					expire: 86400 * 1000 * 30,
 					duration: 86400 * 1000 * 30
 				}
 			},
@@ -148,7 +175,7 @@ Bot.prototype.initialPaymentPlan = function (options, cb) {
 				},
 				programs: ['^[eE]10'],
 				enable: true,
-				visible: true,
+				visible: false,
 				ticket: {
 					expire: 86400 * 1000 * 1,
 					duration: 86400 * 1000 * 1
@@ -156,19 +183,34 @@ Bot.prototype.initialPaymentPlan = function (options, cb) {
 			},
 			{
 				type: 5,
-				title: 'Free',
+				title: 'loginFree',
 				fee: {
 					price: 0,
 					currency: 'USD'
 				},
 				programs: ['^[eE]20'],
 				enable: true,
-				visible: true,
+				visible: false,
 				ticket: {
 					expire: 86400 * 1000 * 1,
 					duration: 86400 * 1000 * 1
 				}
-			}
+			},
+			{
+				type: 6,
+				title: 'adFree',
+				fee: {
+					price: 0,
+					currency: 'USD'
+				},
+				programs: ['^[eE]30'],
+				enable: true,
+				visible: false,
+				ticket: {
+					expire: 86400 * 1000 * 1,
+					duration: 86400 * 1000 * 1
+				}
+			},
 		];
 		self.plans = basicPlans;
 		var collection = self.db.collection('PaymentPlans');
@@ -184,35 +226,42 @@ Bot.prototype.loadPaymentPlan = function (options, cb) {
 	});
 };
 
-Bot.prototype.findPaymentPlan = function (options, cb) {
+/* require: options.uid, options.programs */
+/* playable, ad_to_play, login_to_play, paymentPlans */
+Bot.prototype.fillPaymentInformation = function (options, cb) {
 	cb = dvalue.default(cb, function () {});
 	var self = this, rs;
+	var uid = options.uid;
+	var programs = options.programs || options.program;
 	var fillPlan = function (program) {
 		program.paymentPlans = [];
+		program.playable = false;
+		program.ad_to_play = false;
+		program.login_to_play = false;
 		self.plans.map(function (v) {
-			if(v.programs.some(function (vv) { return new RegExp(vv).test(program.pid); })) { program.paymentPlans.push(v); }
+			if(v.programs.some(function (vv) { return isPlayable(vv, program.pid); })) {
+				if(v.type == 5) { program.login_to_play = true; }
+				else if(v.type == 6) { program.ad_to_play = true; }
+				program.paymentPlans.push(v);
+			}
 		});
 		return program;
 	};
-	if(Array.isArray(options)) {
-		rs = options.map(fillPlan);
-		cb(null, rs);
-		return rs;
+	if(Array.isArray(programs)) {
+		rs = programs.map(fillPlan);
+		//-- check tickets
+		cb(null, programs);
 	}
 	else {
-		rs = fillPlan(options);
-		cb(null, rs);
-		return rs;
+		rs = fillPlan(programs);
+		var options = {uid: options.uid, pid: programs.pid};
+		self.checkPlayable(options, function (e, d) {
+			programs.playable = !!d;
+			cb(null, programs);
+		})
 	}
 };
 
-/* require: options.uid */
-Bot.prototype.getUserTickets = function (options, cb) {
-
-};
-Bot.prototype.checkPlayable = function (options, cb) {
-
-};
 
 /* require: uid, nonce */
 Bot.prototype.createBrainTreeID = function (options, cb) {
@@ -255,7 +304,7 @@ Bot.prototype.createBrainTreeID = function (options, cb) {
 Bot.prototype.fetchBrainTreeID = function (options, cb) {
 	if(!textype.isObjectID(options.uid)) { var e = new Error('user not found'); e.code = '39102'; return cb(e); }
 	var self = this;
-	var condition = {_id: new mongodb.ObjectID(options.uid), BrainTreeID: {$exist: true}};
+	var condition = {_id: new mongodb.ObjectID(options.uid), BrainTreeID: {$exists: true}};
 	var collection = this.db.collection('Users');
 	collection.findOne(condition, {}, function (e, d) {
 		if(e) { e.code = '01002'; return cb(e); }
@@ -306,6 +355,7 @@ Bot.prototype.fetchPrice = function (options, cb) {
 				// 免費影片
 				case 4:
 				case 5:
+				case 6:
 				default:
 					var fee = {
 						price: 0,
@@ -347,7 +397,6 @@ Bot.prototype.order = function (options, cb) {
 /* require: options.nonce, options.oid, options.uid, options.gateway */
 /* gateway: braintree, iosiap */
 Bot.prototype.checkoutTransaction = function (options, cb) {
-	console.log(options);//--
 	if(!textype.isObjectID(options.oid)) { var e = new Error('order not found'); e.code = '39701'; return cb(e); }
 	var self = this;
 	options.gateway = dvalue.default(options.gateway, 'BrainTree').toLowerCase();
@@ -363,15 +412,19 @@ Bot.prototype.checkoutTransaction = function (options, cb) {
 			self.fetchTransactionDetail(options, function (e1, d1) {
 				if(e1) { return cb(e1); }
 				else {
+					var now = new Date().getTime();
 					var receipt = {
 						nonce: options.nonce,
 						gateway: options.gateway,
 						detail: d1
 					};
-					var updateQuery = {$set: {receipt: receipt}};
+					var updateQuery = {$set: {receipt: receipt, mtime: now, atime: now}};
 					collection.findAndModify(condition, {}, updateQuery, {}, function (e2, d2) {
 						if(e2) { e2.code = '01003'; return cb(e2); }
-						else { return cb(null, receipt); }
+						else {
+							self.generateTicket(descOrder(dvalue.default(updateQuery.$set, d)), function () {});
+							return cb(null, receipt);
+						}
 					});
 				}
 			});
@@ -406,12 +459,85 @@ Bot.prototype.fetchTransactionDetail = function (options, cb) {
 	}
 };
 
-Bot.prototype.generateTicket = function () {
+/* require: options.ppid, options.pid, options.oid, options.uid, options.receipt */
+Bot.prototype.generateTicket = function (options, cb) {
+	var paymentPlan = this.plans.find(function (v) {return v.ppid == options.ppid});
+	if(!paymentPlan) { var e = new Error('failed to generate resource ticket'); e.code = '19901'; return cb(e); }
+	var now = new Date().getTime();
+	var ticket = {
+		type: paymentPlan.type,
+		oid: options.oid,
+		uid: options.uid,
+		enable: false,
+		expire: now + paymentPlan.ticket.expire,
+		duration: paymentPlan.ticket.duration,
+		ctime: now,
+		mtime: now,
+		atime: now
+	};
+
+	// ticket detail
+	switch(paymentPlan.type) {
+		case 1:
+			if(!options.receipt || !paymentPlan.programs.some(function (v) { return isPlayable(v, options.pid); })) {
+				var e = new Error('failed to generate resource ticket'); e.code = '19901'; return cb(e);
+			}
+			ticket.programs = [options.pid];
+			break;
+		case 2:
+			ticket.programs = paymentPlan.programs;
+			break;
+		case 3:
+			ticket.programs = paymentPlan.programs;
+			ticket.enable = true;
+			break;
+		case 4:
+		case 5:
+			return cb(null);
+			break;
+		default:
+			var e = new Error('failed to generate resource ticket'); e.code = '19901'; return cb(e);
+	}
+	ticket = formatTicket(ticket);
+	var collection = this.db.collection('Tickets');
+	collection.insert(ticket, {}, function (e, d) {
+		if(e) { e.code = '01001'; return cb(e); }
+		else { return cb(null, ticket); }
+	});
+};
+
+/* require: options.uid */
+Bot.prototype.fetchUserTickets = function (options, cb) {
 
 };
 
-Bot.prototype.openTicket = function () {
+/*  require: options.uid, options.pid */
+Bot.prototype.isFree = function (options) {
+	return this.plans.some(function (v) {
+		return (v.type == 5) && isPlayable(v.programs, options.pid) && textype.isObjectID(options.uid)
+			|| (v.type == 4 || v.type == 6) && isPlayable(v.programs, options.pid);
+	});
+};
 
+/*  require: options.uid, options.pid */
+Bot.prototype.checkPlayable = function (options, cb) {
+	// free program
+	if(this.isFree(options)) { return cb(null, true); }
+	else if(!textype.isObjectID(options.uid)) { return cb(null, false); }
+
+	// check ticket
+	var now = new Date().getTime();
+	var collection = this.db.collection('Tickets');
+	var condition = {uid: options.uid, expire: {$gt: now}, programs: {$in: [options.pid]}};
+	collection.findOne(condition, {}, function (e, d) {
+		if(!d) { return cb(null, false); }
+		else { return cb(null, true); }
+	});
+};
+
+/* require: options.uid, options.pid */
+Bot.prototype.useTicketByProgram = function (options, cb) {
+	var e = new Error('resource access denied'); e.code = '69201'; return cb(e);
 };
 
 module.exports = Bot;
