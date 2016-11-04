19bc46aedbb79eb75d2649497f1005a9a170f5cd
Luphia Chang <luphia@outlook.com>
2016-11-04 10:19:21 +0800

    Update: email sender

--- bots/Mailer.js
+++ bots/Mailer.js
@@ -34,7 +34,7 @@ Bot.prototype.send = function (email, subject, content, cb) {
 	var mailTransport = nodemailer.createTransport(smtpTransport(self.config.mail));
 
 	var mailOptions = {
-		from: 'noreply@isuncloud.com',
+		from: self.config.mail.user,
 		subject: subject,
 		html: content
 	};
